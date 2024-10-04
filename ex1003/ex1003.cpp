d474438f7cf6971d0a7e79b5d423904e7c652cec
JoaoVictorSicco <victordesicco@gmail.com>
2024-10-03 22:37:04 -0400

    Problema 1003: Soma Simples

---
+++ ex1003/ex1003.cpp
@@ -0,0 +1,15 @@
+/* Problema 1003: Soma Simples */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,soma;
+    cin >> a;
+    cin >> b;
+    
+    soma = a + b;
+    cout << "SOMA = " << soma << endl;
+}
